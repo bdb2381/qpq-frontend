e7342cad0369a74d0e384dea2864de732066e8c4
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-25 09:04:37 -0700

    changee json.files port to 3001

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>gem install bundler -v 2.0.2
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
